c0608fa3edac2749045a5cf8ef26810df64cbb07
linbei-pro <148841405+linbei-pro@users.noreply.github.com>
2024-02-20 14:39:21 +0800

    Add files via upload

--- text.c
+++ text.c
@@ -3,16 +3,18 @@
 #define N 11
 int main() {
 	int arr[N] = { 1,2,10,3,4,5,6,7,8,9,7 };
-	int i = 0, x1 = 0;
-	for (i = 1; i < N; i++)
+	int new_arr[N] = { 0 };
+	int i = 0;
+	for (i = 0; i < N; i++)
 	{
-		x1 = x1 ^ i;//Òì»ò1µ½10
+		new_arr[arr[i]]++;
 	}
 	for (i = 0; i < N; i++)
 	{
-		x1 = x1 ^ arr[i];
+		if (new_arr[i] == 2)
+			printf("%d",i);
 	}
-	printf("%d \n", x1);
+
 
 	return 0;
 }
