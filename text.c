9eb784c887d12fb763405a2f8044c1bdb22c95df
linbei-pro <148841405+linbei-pro@users.noreply.github.com>
2024-02-20 14:47:59 +0800

    Update text.c

--- text.c
+++ text.c
@@ -3,6 +3,19 @@
 #define N 11
 int main() {
 	int arr[N] = { 1,2,10,3,4,5,6,7,8,9,7 };
+	//方法一
+	int x1 = 0, j = 0;
+	for (j = 1; j <=N - 1; j++)
+	{
+		x1 = x1 ^ j;
+	}
+	for (j = 0; j < N; j++)
+	{
+		x1 = x1 ^ arr[j];
+	}
+	printf("%d \n", x1);
+	//方法二
+	printf("============\n");
 	int new_arr[N] = { 0 };
 	int i = 0;
 	for (i = 0; i < N; i++)
@@ -12,9 +25,7 @@ int main() {
 	for (i = 0; i < N; i++)
 	{
 		if (new_arr[i] == 2)
-			printf("%d",i);
+			printf("%d ", i);
 	}
-
-
 	return 0;
 }
